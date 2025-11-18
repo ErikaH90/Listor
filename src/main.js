6f8b48cb02202df4de7fb98d43dd0472a68ffa0c
Erika Hörling <erikahorling@mbpsomtorerika2.home>
2025-11-18 14:23:10 +0100

    loop 1

--- src/main.js
+++ src/main.js
@@ -1,10 +1,23 @@
 const nameInput = document.getElementById("name");
+const lastnameInput = document.getElementById("lastname");
 const text = document.getElementById("length");
+const text2 = document.getElementById("letters");
 const button = document.getElementById("button");
+const button2 = document.getElementById("button2");
 
 button.addEventListener ("click", (event) => {
   event.preventDefault();
   const textLength = nameInput.value.length;
-  text.innerText = "Namnet är " + textLength " bokstäver långt.";
+  text.innerText = "Namnet är " + textLength + " bokstäver långt.";
 });
+
+button2.addEventListener ("click", (event) => {
+  event.preventDefault();
+  const lastname = lastnameInput.value.split("");
+  text2.innerHTML = "Bokstäverna är \n";
+  for (let i = 0; i < lastname.length; i++) {
+  text2.innerHTML += lastname[i] + " ";
+  }
+});
+
 
